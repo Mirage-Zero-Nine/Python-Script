71531dc3db29e279ce1159094cc444147f5c87a1
Mirage09 <borismirage00@gmail.com>
2017-09-02 19:28:44 -0400

    Remove redundant part

--- LaunchbarPython/currency.py
+++ LaunchbarPython/currency.py
@@ -24,41 +24,10 @@ import requests
 def currency():
     currencySession = requests.session()
     currencyRespond = currencySession.get('https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote')
-
     root = ElementTree.fromstring(currencyRespond.content)
-    node = root.findall('./resources/resource/field')
-    print node[435].text
-
-    '''
-    result = float(node[434])*float(sys.argv[1])
+    node = root.findall('./resources/resource/field') # This line is to find current rate for USD/CNY. Any other rate can be found on this website
+    result = float(node[435])*float(sys.argv[1])
     print '%.2f' % result
-    
-    
-    i = 0
-    while True:
-        if node[i].text != 'USD/CNY':
-            i +=1
-        else:
-            print i
-            break
-
-
-    
-    for resource in root[1]:
-        for name in resource:
-            print type(name)
-            print name.text
-    if tag.text == 'USD/CNY':
-        print root[1][0][i]
-        break
-    else:
-        i += 1
-    
-    
-    result = float(sys.argv[1])*currencyRespond
-    print '%.2f' % currencyRespond
-    
-    '''
 
 
 if __name__ == '__main__':
