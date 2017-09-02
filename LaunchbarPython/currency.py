deef836592e26d5cd312612d68fd1fbbd2f6e777
Mirage09 <borismirage00@gmail.com>
2017-09-02 16:14:54 -0400

    Use a new api

--- LaunchbarPython/currency.py
+++ LaunchbarPython/currency.py
@@ -3,6 +3,7 @@
 # LaunchBar Action Script
 #
 import sys
+from xml.etree import ElementTree
 
 items = []
 
@@ -22,9 +23,43 @@ import requests
 
 def currency():
     currencySession = requests.session()
-    currencyRespond = currencySession.get('http://apilayer.net/api/live?access_key=8f98bbab6dc62d96b2846c5a9e8e592b').json()['quotes']['USDCNY']
-    result = float(sys.argv[1])*currencyRespond
+    currencyRespond = currencySession.get('https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote')
+
+    root = ElementTree.fromstring(currencyRespond.content)
+    node = root.findall('./resources/resource/field')
+    print node[435].text
+
+    '''
+    result = float(node[434])*float(sys.argv[1])
     print '%.2f' % result
+    
+    
+    i = 0
+    while True:
+        if node[i].text != 'USD/CNY':
+            i +=1
+        else:
+            print i
+            break
+
+
+    
+    for resource in root[1]:
+        for name in resource:
+            print type(name)
+            print name.text
+    if tag.text == 'USD/CNY':
+        print root[1][0][i]
+        break
+    else:
+        i += 1
+    
+    
+    result = float(sys.argv[1])*currencyRespond
+    print '%.2f' % currencyRespond
+    
+    '''
+
 
 if __name__ == '__main__':
     currency()
