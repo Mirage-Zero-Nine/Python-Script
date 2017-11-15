54b88a4cc3b9808fbfb84e5fc2eed9d2e8532d0d
Mirage09 <borismirage00@gmail.com>
2017-11-15 16:05:54 -0500

    Add file

---
+++ LaunchbarPython/random_string.py
@@ -0,0 +1,30 @@
+__author__ = 'BorisMirage'
+# --- coding:utf-8 ---
+#!/usr/bin/env python
+#
+# LaunchBar Action Script
+#
+import sys
+import json
+import time
+import hashlib
+import subprocess
+import random
+from passlib.hash import sha512_crypt
+
+
+class random_string(object):
+    def __init__(self, base_string):
+        self.string = base_string
+
+    def generate_random(self):
+        hash_string = hashlib.sha224(self.string).hexdigest()
+        code = sha512_crypt.encrypt(hash_string, rounds=5000, salt=str(random.randint(0, 100)))
+        code = code[12:21]
+        process = subprocess.Popen('pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
+        process.communicate(code.encode('utf-8'))
+        print code
+
+if __name__ == '__main__':
+    random_string(str(time.localtime())).generate_random()
+
