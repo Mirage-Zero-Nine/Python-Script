0d5f703a9ed726725f4263274fdec047d546b28b
Mirage09 <borismirage00@gmail.com>
2018-01-21 15:18:58 -0500

    Add comment

--- LaunchbarPython/express.py
+++ LaunchbarPython/express.py
@@ -29,14 +29,23 @@ for arg in sys.argv[1:]:
 def express():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30',
-    }  # Change headers!
+    }
+
+    # Express id
     num = str(sys.argv[1])
+
+    # Express request URL
     url = 'https://www.kuaidi100.com/autonumber/autoComNum?text='
     url = url + num
+
+    # Set up session
     sessionExpress = requests.Session()
     sessionExpress.headers.update(headers)
-    # sessionPost_1 = sessionExpress.post('https://www.kuaidi100.com/autonumber/autoComNum?text=')
+
+    # Post
     sessionPost = sessionExpress.post(url)
+
+    # Filter the result
     type = re.findall(r'(?<=comCode":")\w+', sessionPost.text)
     i = 0
     while True:
@@ -54,6 +63,8 @@ def express():
     # getUrl = 'https://www.kuaidi100.com/query?type=' + str(type[0]) + '&postid=' + num
     # getResult = sessionExpress.get(getUrl)
     # getResult.encoding = 'uft-8'
+
+    # Format data and print it out
     getResult = getResult.json()
     i = 0
     while i < len(getResult['data']):
@@ -65,6 +76,7 @@ def express():
         content = getResult['data'][i]['context'].encode('utf-8')
         print(time + content)
         i += 1
+        
         # post https://www.kuaidi100.com/autonumber/autoComNum?text=
         # expressRespond = sessionExpress.get('https://www.kuaidi100.com/query?type=shunfeng&postid=').json()
         # expressRespond = json.dumps(expressRespond, ensure_ascii=False, indent=4)
